6a6006c25392988f56980ea41074f185216c552c
Dev-Castillo <143166266+Dev-Castillo@users.noreply.github.com>
2024-06-06 15:44:54 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import './App.css'
+const DrumMachine = () => {
+  const [display, setDisplay] = useState('');
+
+  const handleClick = (clip) => {
+    setDisplay(clip.id);
+    const audio = document.getElementById(clip.keyTrigger);
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  const handleKeyPress = (e) => {
+    const clip = audioClips.find((clip) => clip.keyCode === e.keyCode);
+    if (clip) {
+      setDisplay(clip.id);
+      const audio = document.getElementById(clip.keyTrigger);
+      audio.currentTime = 0;
+      audio.play();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  });
+
+  return (
+    <div id="drum-machine" className="container">
+      <div id="display">{display}</div>
+      <div className="drum-pads">
+        {audioClips.map((clip) => (
+          <div
+            key={clip.id}
+            className="drum-pad"
+            id={clip.id}
+            onClick={() => handleClick(clip)}
+          >
+            {clip.keyTrigger}
+            <audio
+              className="clip"
+              id={clip.keyTrigger}
+              src={clip.url}
+            ></audio>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const audioClips = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
+
+export default DrumMachine;
